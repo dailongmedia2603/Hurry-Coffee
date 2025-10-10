ba3e85bf5cf6e97075086ec7267fa547ab257039
[dyad] <huulong111@gmail.com>
2025-10-10 10:32:57 +0700

    [dyad] Cấu hình PWA cho ứng dụng - wrote 2 file(s), deleted 1 file(s)

---
+++ pwa.config.js
@@ -0,0 +1,4 @@
+// This file enables and configures the Progressive Web App (PWA) service worker.
+// Its presence triggers the generation of the service worker during the web build process,
+// allowing the app to be installed on devices and work offline.
+module.exports = {};
