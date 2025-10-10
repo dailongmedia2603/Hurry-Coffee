daaad1398b8be37d85115c2dda7d6b7f3842e7a6
[dyad] <huulong111@gmail.com>
2025-10-10 11:06:17 +0700

    [dyad] Sửa lỗi PWA và deploy trên Vercel - wrote 3 file(s), deleted 3 file(s)

--- app/+html.tsx
+++ app/+html.tsx
@@ -13,7 +13,6 @@ export default function Html({ children }: { children: ReactNode }) {
         
         {/* PWA Configuration */}
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
         <meta name="theme-color" content="#73509c" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
