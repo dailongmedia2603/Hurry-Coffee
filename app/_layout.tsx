16ec9ff82e0421644287cd2e54addeb80d377dd6
[dyad] <huulong111@gmail.com>
2025-10-14 01:30:07 +0700

    [dyad] Sửa lỗi PWA không cài đặt được - wrote 1 file(s), deleted 1 file(s) + extra files edited outside of Dyad

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,7 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "@/src/context/AuthContext";
-import { useEffect } from 'react';
-import { register } from '@/src/utils/registerServiceWorker';
 
 export default function RootLayout() {
-  useEffect(() => {
-    register();
-  }, []);
-
   return (
     <AuthProvider>
       <Stack screenOptions={{ headerShown: false }} />
